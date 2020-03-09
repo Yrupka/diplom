b5a6301bcbacbb1066c2369c56f36e8337fc3981
Алекс <@DESKTOP-ASGU9JN>
2020-03-09 17:36:02 +0700

    Добавление 3д
    Добавление весов и топливного контейнера. Скрипт для подсвечивания объекта.

--- Diplom/Assets/Scripts/Engine_cam.cs
+++ Diplom/Assets/Scripts/Engine_cam.cs
@@ -3,37 +3,52 @@ using System.Collections;
 
 public class Engine_cam : MonoBehaviour
 {
-    public GameObject go;
-    public float sensitivity = 1F;
-    private Camera goCamera;
-    private Vector3 MousePos;
-    private float MyAngle = 0F;
+
+    // Присваиваем переменные
+    public float mouseSensitivity = 10f;
+    public float speed = 5f;
+    private Vector3 transfer;
+    public float minimumX = -360f;
+    public float maximumX = 360f;
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+    float rotationX = 0f;
+    float rotationY = 0f;
+    Quaternion originalRotation;
+
 
     void Start()
     {
-        // Инициализируем кординаты мышки и ищем главную камеру
-        goCamera = Camera.main;
-        //go = goCamera.transform.parent.gameObject;
+        originalRotation = transform.rotation;
     }
 
     void Update()
     {
-        MousePos = Input.mousePosition;
-    }
+        // Движения мыши -> Вращение камеры
+        rotationX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+        transform.rotation = originalRotation * xQuaternion * yQuaternion;
 
-    void FixedUpdate()
-    {
-        if (Input.GetMouseButton(1))
-        {
-            MyAngle = 0;
-            // расчитываем угол, как:
-            // разница между позицией мышки и центром экрана, делённая на размер экрана
-            //  (чем дальше от центра экрана тем сильнее поворот)
-            // и умножаем угол на чуствительность из параметров
-            MyAngle = sensitivity * ((MousePos.x - (Screen.width / 2)) / Screen.width);
-            goCamera.transform.RotateAround(go.transform.position, goCamera.transform.up, MyAngle);
-            MyAngle = sensitivity * ((MousePos.y - (Screen.height / 2)) / Screen.height);
-            goCamera.transform.RotateAround(go.transform.position, goCamera.transform.right, -MyAngle);
-        }
+        //// Ускорение при нажатии клавиши Shift
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //    speed *= 10;
+        //else if (Input.GetKeyUp(KeyCode.LeftShift))
+        //    speed /= 10;
+
+        //// Поднятие и опускание камеры
+        //Vector3 newPos = new Vector3(0, 1, 0);
+        //if (Input.GetKey(KeyCode.E))
+        //    transform.position += newPos * speed * Time.deltaTime;
+        //else if (Input.GetKey(KeyCode.Q))
+        //    transform.position -= newPos * speed * Time.deltaTime;
+
+        // перемещение камеры
+        transfer = transform.forward * Input.GetAxis("Vertical");
+        transfer += transform.right * Input.GetAxis("Horizontal");
+        transform.position += transfer * speed * Time.deltaTime;
     }
 }
---
+++ Diplom/Assets/Scripts/Objects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Objects : MonoBehaviour
+{
+    private void Awake()
+    {
+        Set_highlite(Color.clear);
+    }
+
+    private void OnMouseEnter()
+    {
+        Set_highlite(Color.blue);
+    }
+    private void OnMouseExit()
+    {
+        Set_highlite(Color.clear);
+    }
+
+    private void Set_highlite(Color color)
+    {
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", color);
+    }
+}
